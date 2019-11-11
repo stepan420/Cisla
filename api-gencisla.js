9734e4e757d9399cb49f0f89aa9ff4943316824e
tupy <tupy@porg.cz>
2019-11-11 17:48:15 +0100

    B

--- api-gencisla.js
+++ api-gencisla.js
@@ -1,12 +1,33 @@
+let obj = {};
+obj.odp = "";
 exports.apiGenCisla = function(req, res, q) {
     res.writeHead(200, {"Content-type": "application/json", "Access-Control-Allow-Origin": "*"});
-    let obj = {};
-    if (q.query["max"] && q.query["min"]) {
-        let max = q.query["max"];
-        let min = q.query["min"];
-        let n = Math.random();
-        n = +min + Math.trunc((max-min+1)*n);
-        obj.cislo = n;
+    if (q.pathname === "/gencisla/nove"){
+        if (q.query["max"] && q.query["min"]) {
+            let max = q.query["max"];
+            let min = q.query["min"];
+            let n = Math.random();
+            n = +min + Math.trunc((max - min + 1) * n);
+            obj.cislo = n;
+        }
     }
-    res.end(JSON.stringify(obj))
+    else if (q.pathname.endsWith("/gencisla/kontrola")){
+        if (q.query["guess"]){
+            let g = q.query["guess"];
+            let n = obj.cislo;
+            if (g>n){
+                obj.vys = "Moc";
+                obj.odp = obj.odp + g + " > Hledané číslo<br>"
+            }
+            else if (g<n){
+                obj.vys = "Málo";
+                obj.odp = obj.odp + g + " < Hledané číslo<br>"
+            }
+            else {
+                obj.vys = "SPRÁVNĚ";
+                obj.odp = ""
+            }
+        }
+    }
+    res.end(JSON.stringify(obj));
 };
--- cisla.js
+++ cisla.js
@@ -2,38 +2,26 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const apiGenCisla = require("./api-gencisla").apiGenCisla;
-const apiKontCisla = require("./api-kontcisla").apiKontCisla;
 
-function processStaticFiles(res, fileName) {
+function processStaticFiles(res, fileName){
     fileName = fileName.substr(1);
     console.log(fileName);
     let contentType = "text/html";
-    if (fileName.endsWith(".png")) {contentType = "image/png"}
-    else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")) {contentType = "image/jpeg";}
-    if (fs.existsSync(fileName)) {
-        fs.readFile(fileName, function(err, data) {
+    if (fileName.endsWith(".png")){contentType = "image/png"}
+    else if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg")){contentType = "image/jpeg"}
+    if (fs.existsSync(fileName)){
+        fs.readFile(fileName, function(err, data){
             res.writeHead(200, {'Content-Type': contentType});
             res.write(data);
             res.end();
-        });
-    }
-    else {
-        res.writeHead(404);
-        res.end();
-    }
+        })
+    } else{res.writeHead(404); res.end()}
 }
 http.createServer((req, res) => {
     let q = url.parse(req.url, true);
-    if (q.pathname === "/") {
-        processStaticFiles(res, "/cisla.html");
-        return;
-    }
-    if (q.pathname.length - q.pathname.lastIndexOf(".") < 6) {
-        processStaticFiles(res, q.pathname);
-        return;
-    }
-    else if (q.pathname === "/gencisla") {apiGenCisla(req, res, q)}
-    else if (q.pathname === "/kontcisla") {apiKontCisla(req, res, q)}
+    if (q.pathname === "/"){processStaticFiles(res, "/cisla.html");return}
+    if (q.pathname.length - q.pathname.lastIndexOf(".") < 6){processStaticFiles(res, q.pathname);return}
+    else if (q.pathname.startsWith("/gencisla")){apiGenCisla(req, res, q)}
     else{
         res.writeHead(200, {"Content-type":"text/html"});
         res.end("<html style='font-family: Calibri; font-size: 500px' lang='cs'><head><meta charset='UTF8'><title></title></head><body></body></html>")
